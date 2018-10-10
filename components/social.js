6b86865cf089c93000ea1c088f0d11bf284823c6
mortuz <mortuzalam@gmail.com>
2018-10-10 18:43:59 +0530

    add components

---
+++ components/social.js
@@ -0,0 +1,19 @@
+document.write(`
+    <div class="social-media-icons d-none d-lg-block">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 text-right">
+                    <a href="#" target="_blank">
+                        <i class="fa fa-facebook-f"></i>
+                    </a>
+                    <a href="https://twitter.com/nisargapackagi1" target="_blank">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                    <a href="https://www.instagram.com/nisargapackaging/" target="_blank">
+                        <i class="fa fa-instagram"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+`);
